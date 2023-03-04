91325388f451181d8ecb10ab044423d6cacd842b
Papiricoh <slendeman0200@gmail.com>
2023-03-05 00:53:26 +0100

    Added WordRenderer.java

---
+++ src/game/WordRenderer.java
@@ -0,0 +1,51 @@
+package game;
+
+import java.security.InvalidParameterException;
+
+public class WordRenderer {
+    private char[] orWord;
+    private char[] renderedWord;
+
+    public WordRenderer(Word originalWord) {
+        this.orWord = new char[originalWord.getFullWord().length()];
+        String fullWord = originalWord.getFullWord();
+        for (int i = 0; i < orWord.length; i++) {
+            this.orWord[i] = fullWord.charAt(i);
+        }
+        this.renderedWord = new char[originalWord.getFullWord().length()];
+        for (int i = 0; i < this.renderedWord.length; i++) {
+            this.renderedWord[i] = '_';
+        }
+    }
+
+    public String getRenderWord() {
+        String result = "";
+        for (int i = 0; i < this.renderedWord.length; i++) {
+            result += this.renderedWord[i];
+        }
+        return result;
+    }
+
+    public String tryWord(String word) throws InvalidParameterException {
+        if(word.length() != this.orWord.length) {
+            throw new InvalidParameterException("The Word must be " + this.orWord.length + " in length");
+        }
+        for (int i = 0; i < this.orWord.length; i++) {
+            if(this.orWord[i] == word.charAt(i)) {
+                this.renderedWord[i] = this.orWord[i];
+            }else {
+                this.renderedWord[i] = checkChar(word.charAt(i));
+            }
+        }
+        return getRenderWord();
+    }
+
+    private char checkChar(char charAt) {
+        for (int i = 0; i < this.orWord.length; i++) {
+            if(this.orWord[i] == charAt) {
+                return '*';
+            }
+        }
+        return '_';
+    }
+}
