57a33fc6da6e1c6775c522bd51173622d760d63f
Papiricoh <slendeman0200@gmail.com>
2023-03-05 01:08:48 +0100

    Added gameMenu() to GameManager.java

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,9 @@ import leaderboard.Leaderboard;
 public class Main {
 
 	public static void main(String[] args) {
-		FileManager fm = new FileManager("database.txt");
-		Leaderboard leaderboard = new Leaderboard(Parser.parseLeaderboard(fm.getRawData()));
-		System.out.println(leaderboard.toString());
-		System.out.println(Parser.unParseLeaderboard(leaderboard.getArrayLeaderboard()));
 		GameManager gm = new GameManager("words.txt");
 		System.out.println(gm.toString());
+		gm.gameMenu();
 	}
 
 }
--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -2,8 +2,10 @@ package game;
 
 import fileManager.FileManager;
 import fileManager.Parser;
+import leaderboard.Leaderboard;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GameManager {
     private Word[] words;
@@ -11,6 +13,35 @@ public class GameManager {
     public GameManager(String route) {
         FileManager fm = new FileManager(route);
         this.words = Parser.parseWords(fm.getRawData());
+    }
+
+    public void gameMenu() {
+        Scanner scan = new Scanner(System.in);
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("WORDLE\n1: Start game\n2: Leaderboard\n3: Exit");
+            System.out.print("Select option");
+            int selection = scan.nextInt();
+            switch (selection) {
+                case 1:
+                    startGame(scan);
+                    break;
+                case 2:
+                    FileManager fm = new FileManager("database.txt");
+                    //TODO Short rawData
+                    System.out.println(fm.getRawData());
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+            }
+        }
+
+        scan.close();
+    }
+
+    private void startGame(Scanner scan) {
+
     }
 
     @Override
