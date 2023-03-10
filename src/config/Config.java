e2fb148629bbd26c46c0d898d7a2abaaea5dab4d
papiricoh <slendeman0200@gmail.com>
2023-03-10 10:05:08 +0100

    Fixed wordWrap

--- src/config/Config.java
+++ src/config/Config.java
@@ -2,9 +2,8 @@ package config;
 
 public class Config {
     public static final char[] VOWELS = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
-    public static final char[] CONSONANTS = {'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z',
-            'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
-    public static final int TRIES = 5;
+    public static final char[] CONSONANTS = {'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+    public static final int TRIES = 40;
     public static final int CONSONANT_POINTS = 10;
     public static final int VOWEL_POINTS = 5;
 }
